24eb75aed1710fe54bb9b1f637bd2ba915b2afb9
ArnauCatPT <222076608+ArnauCatPT@users.noreply.github.com>
2025-10-30 19:18:26 +0100

    no idea

--- estadisticas.py
+++ estadisticas.py
@@ -1,6 +1,7 @@
 def media_notas(dict):
     return sum(dict.values())/len(dict)
 
+
 def max_notas(dict):
     notas = [nota for fila, nota in dict.items()]
     return print(f"Nota más alta: {max(notas)}")
